0b551d9d6f06f1adb5c2eb9d9185becb1805cffd
ss <kxg18983916968@163.com>
2026-01-03 11:54:37 +0800

    Initial Commit

---
+++ EarthLord/EarthLordApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthLordApp.swift
+//  EarthLord
+//
+//  Created by yanshuang ren on 2026/1/3.
+//
+
+import SwiftUI
+
+@main
+struct EarthLordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
